4cfb6712ab63ec340e9ee577bf4803a6055badc8
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-11-03 02:48:47 -0500

    adds changes to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,19 @@
 class OrdersController < ApplicationController
+
+    def create
+       new_order= Order.create(order_params)
+       render json: new_order
+    end
+
+    def destroy
+        order= Order.find(params[:id])
+        order.destroy
+        render json: order
+    end
+
+    private
+
+    def order_params
+        params.permit(:product_id, :cart_id, :quantity)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :orders
+  # resources :orders
   resources :products
   resources :categories
   resources :carts
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   get '/keep_logged_in', to: "users#keep_logged_in"
   post '/login', to: "users#index"
+  post '/orders', to:  "orders#create"
+  delete '/orders/:id', to: "orders#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
